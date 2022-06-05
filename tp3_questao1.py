2b4c0274c92d8130697f7f0fe1a2d2c07225ade9
VicTroia-Dev <100329583+VicTroia-Dev@users.noreply.github.com>
2022-06-05 20:41:49 -0300

    Atualização da TP3

--- tp3_questao1.py
+++ tp3_questao1.py
@@ -4,8 +4,6 @@
 #Laço para a execução de aplicação:
 while True:
 
-    try:
-
         #Valor total gasto
         total = float(input("Informe o valor do consumo: R$ "))
 
@@ -30,31 +28,6 @@ while True:
                     totalConsumido = ("%0.2f" % totalConsumido).replace(".", ",")
                     totalConsumidores = ("%0.2f" % totalConsumidores).replace(".", ",")
 
-                    #Imprimir na tela o resultado da função 
-                    print(f"O valor total da conta, com a taxa de serviço, será de R$ {totalConsumido}.\n")
-                    print(f"Dividindo a conta por {consumidores} pessoas, cada cliente deve pagar R$ {totalConsumidores}.")
-
-                #Erro na porcentagem
-                else:
-                    print("A porcentagem deve ser entre '0' e '100'.")
-
-            #Erro no número de clientes
-            else:
-                print("Valor inválido. Necessário o mínimo de 1 pessoa.")
-
-        #Erro de consumo
-        else:
-            print("Você precisa ter consumido algum produto do bar!")
-
-
-
-        print("\n-------------------------------------------------------------------------------------------------------------")
-        continuidade = input("Deseja continuar calculando? \nPara continudar pressione 'Enter'. Caso deseje finalizar operação, apressione 'X'.")
-
-        if continuidade == "x" or continuidade == "X":
-            print("Finalizando a operação....")
-            print("\nFinalizado! Obrigado pela preferência")
-            break
-
-    except ValueError:
-        print("\n Entrada Inválida \n")
+        #Imprimir na tela o resultado da função 
+        print(f"\nO valor total da conta, com a taxa de serviço, será de R$ {totalConsumido}.")
+        print(f"Dividindo a conta por {consumidores} pessoas, cada cliente deve pagar R$ {totalConsumidores}.")
--- tp3_questao2.py
+++ tp3_questao2.py
@@ -2,7 +2,7 @@
 se essa pessoa é: um eleitor obrigatório, facultativo ou não tem direito ao voto."""
 
 while True:
-    try:
+    
         idade = int(input("Informe sua idade, por favor: "))
         
         if idade >= 18 and idade < 70:
@@ -15,18 +15,4 @@ while True:
 
                  if idade <16:
                      print("Não tem direito ao voto.")
-
-        print("\n------------------------------------------------------------")
-        continuidade = input("Deseja prosseguir avaliando seu status de voto? \nEntão pressione 'Enter'. Caso queria encerrar operação, pressione 'X'.")
-
-        if continuidade == "X" or continuidade =="x":
-            print("Finalizando a operação....")
-            print("\nFinalizado! Parabéns pela análise, exerça seu direito democrático.")
-            break
-        else:
-            print("\n------------------------------------------------------------")
-            continue
-
-    except ValueError:
-        print("\n Entrada Inválida \n")
             
